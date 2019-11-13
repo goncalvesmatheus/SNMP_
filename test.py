b7888f0d19b57913ee1a96ed30b89b789b17364a
Matheus Goncalves <ms.goncalves98@gmail.com>
2019-11-13 19:47:54 +0000

    Start project

---
+++ test.py
@@ -0,0 +1,12 @@
+from pysnmp import hlapi
+def get(target, oids, credentials, port=161, engine=hlapi.SnmpEngine(), context=hlapi.ContextData()):
+    handler = hlapi.getCmd(
+        engine,
+        credentials,
+        hlapi.UdpTransportTarget((target, port)),
+        context,
+        *construct_object_types(oids)
+    )
+    return fetch(handler, 1)[0]
+
+    
